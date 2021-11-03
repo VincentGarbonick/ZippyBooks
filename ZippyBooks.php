5a7ee73fccc923cd955868164fa15fc82f0a881e
Vinnie-Windows <vrg6@uakron.edu>
2021-11-02 23:39:17 -0400

    get popups working, will have to use easy JS popup because having too many popups hidden causes spacing issues.

---
+++ ZippyBooks.php
@@ -0,0 +1,130 @@
+<html>
+<head>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Tangerine">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
+    <link rel="stylesheet" href="styleAndScripts/css/ZippyBooksStyle.css" type="text/css">
+    <link rel="stylesheet" href="styleAndScripts/css/bootstrap.css" type="text/css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="styleAndScripts/css/style.css" type="text/css">
+</head>
+<body>
+    <div class="left-div"> 
+        <div class="d-flex justify-content-md-center align-items-center vh-100"> 
+            <div>
+                <p class="title"> ZippyBooks </p>
+                <form>
+                    <button type="button" class="btn btn-primary" id="">Print Books</button>
+                    <button type="button" class="btn btn-primary" id="contact-icon">Add New Book</button>
+                    <!--<button type="button" class="btn btn-primary" id="contact-changePrice" name ="contact-changePrice">Change Price</button> -->
+                    <button type="button" class="btn btn-primary" id="contact-nukeButton" name ="contact-nukeButton">Change Price</button>
+                    <button type="button" class="btn btn-primary" id="" name="">Delete Book</button>
+                </form>
+            </div>
+        </div>
+    </div>
+    <div class="right-div"> 
+    </div>
+    <div id="contact-popup">
+        <form class="contact-form" action="getInfo.php" id="contact-form" method="post" enctype="multipart/form-data">
+            <h1>Enter New Book</h1>
+            <div>
+                <div>
+                    <label>Title: </label>
+                    <span id="firstName-info" class="info"></span>
+                </div>
+                <div>
+                    <input type="text" id="firstName" name="firstName" class="inputBox" />
+                </div>
+            </div>
+            <div>
+                <div>
+                    <label>Prince: </label>
+                    <span id="lastName-info" class="info"></span>
+                </div>
+                <div>
+                    <input type="text" id="lastName" name="lastName" class="inputBox" />
+                </div>
+            </div>
+            
+            <div>
+                <div>
+                    <label>Quantity: </label><span id="userEmail-info" class="info"></span>
+                </div>
+                <div>
+                    <input type="text" id="userEmail" name="userEmail" class="inputBox" />
+                </div>
+            </div>
+            <div>
+                
+                <!--<input type="submit" id="sendUser" name="sendUser" value="Send" />-->
+                <button type="submit" class="btn btn-primary" id="sendUser" name="sendUser">Submit</button>
+                <button type="button" id="closePopup" class="btn btn-danger">Close</button>
+            </div>
+        </form>
+    </div>
+
+    <div id="contact-nuke-popup">
+        <form class="contact-form" action="getInfo.php" id="contact-form" method="post" enctype="multipart/form-data">
+            <h1>Delete Book</h1>
+            <div>
+            <div>
+                <div>
+                    <label>Book Title: </label>
+                    <span id="email-info" class="info"></span>
+                </div>
+                <div>
+                    <input type="text" id="email" name="email" class="inputBox" />
+                </div>
+            </div>
+                <!--<input type="submit" id="sendUser" name="sendUser" value="Send" />-->
+                <button type="submit" class="btn btn-primary" id="nukeUser" name="nukeUser">Delete Book</button>
+                <button type="button" id="closeNukePopup" class="btn btn-danger">Close</button>
+            </div>
+        </form>
+    </div>
+
+
+    
+</body>
+</html>
+
+<script>
+    $(document).ready(function () 
+    {
+        // show contact form on click 
+        $("#contact-icon").click(function () 
+        {
+            $("#contact-popup").show();
+        });
+
+        $("#contact-nukeButton").click(function () 
+        {
+            $("#contact-nuke-popup").show();
+        });
+        
+        $("#contact-changePrice").click(function () 
+        {
+            $("#contact-change-popup").show();
+        });
+
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////        
+        
+        // hide contact form on click 
+        $("#closePopup").click(function () 
+        {
+            $("#contact-popup").hide();
+        });
+
+        $("#closeNukePopup").click(function () 
+        {
+            $("#contact-nuke-popup").hide();
+        });
+
+        $("#closeChangePrice").click(function () 
+        {
+            $("#contact-change-popup").hide();
+        });
+    }
+    );
+</script>
---
+++ testingII.php
@@ -0,0 +1,160 @@
+<html>
+<head>
+    <title>Test</title>
+</head>
+<body>
+    <h1>ZippyBooks</h1>
+    <form action="testing.php" method = "post">
+        <h3> Form for Adding Entry </h3>
+        <table>
+          <tr>
+            <th> Title </th>
+            <th> Price </th>
+            <th> Qty </th>
+          </tr>
+          <tr>
+            <td><input type = "text"  name = "addform_Title" size = "16" value = "Arguments For Utilitarianism" /></td>
+            <td><input type = "text"  name = "addform_Price" size = "8" value = "5.67" /></td>
+            <td><input type = "text"  name = "addform_Qty" size = "4" value = "4" /></td>
+          </tr>
+      </table>
+      <h3> Form for changing price </h3>
+      <table>
+          <tr>
+            <th> Title </th>
+            <th> Price </th>
+          </tr>
+          <tr>
+            <td><input type = "text"  name = "priceform_Title" size = "16" value = "Arguments For Utilitarianism" /></td>
+            <td><input type = "text"  name = "priceform_Price" size = "8" value = "5.67" /></td>
+          </tr>
+      </table>
+      <h3> Form for deleting record </h3>
+      <table>
+          <tr>
+            <th> Title </th>
+          </tr>
+          <tr>
+            <td><input type = "text"  name = "deleteform_Title" size = "16" value = "Arguments For Utilitarianism" /></td>
+          </tr>
+      </table>
+        <div style="border:1px solid red; display:inline-block;">
+            <h3 style="color:red"> Buttons For Submission </h3>
+            <input type="submit" name="btn-print" value="Print Whole Table">
+            <input type="submit" name="btn-add" value="Add New Entry">    
+            <input type="submit" name="btn-price" value="Change Price">  
+            <input type="submit" name="btn-delete" value="Delete Price">      
+        </div>
+    </form>
+</body>
+</html>
+
+<?php
+    function makeTable($conn, $query)
+    {
+        $result = mysqli_query($conn,$query); 
+        // Get the number of rows in the result, as well as the first row
+        //  and the number of fields in the rows
+        $num_rows = mysqli_num_rows($result);
+        //print "Number of rows = $num_rows <br />";
+
+        print "<table><caption> <h2> Cars ($num_rows) </h2> </caption>";
+        print "<tr align = 'center'>";
+
+        $row = mysqli_fetch_array($result);
+        $num_fields = mysqli_num_fields($result);
+
+        // Produce the column labels
+        $keys = array_keys($row);
+        for ($index = 0; $index < $num_fields; $index++) 
+            print "<th>" . $keys[2 * $index + 1] . "</th>";
+        print "</tr>";
+
+        // Output the values of the fields in the rows
+        for ($row_num = 0; $row_num < $num_rows; $row_num++) {
+            print "<tr align = 'center'>";
+            $values = array_values($row);
+            for ($index = 0; $index < $num_fields; $index++){
+                $value = htmlspecialchars($values[2 * $index + 1]);
+                print "<th>" . $value . "</th> ";
+            }
+            print "</tr>";
+            $row = mysqli_fetch_array($result);
+        }
+        print "</table>";
+    }
+    $hostName = "localhost";
+    $userName = "root";
+    $password = "";
+    $dbName = "isp";
+
+    $conn = mysqli_connect($hostName, $userName, $password,$dbName);
+    if(isset($_POST['btn-print']))
+    {
+        makeTable($conn, "SELECT * FROM zippybooksdb");
+    }
+    elseif(isset($_POST['btn-add']))
+    {
+        $title = $_POST["addform_Title"];
+        $price = $_POST["addform_Price"];
+        $qty = $_POST["addform_Qty"];
+
+        // get largest ID, so we can increment it
+        // this would obviously not work in real life with asycnrhonous usecases, and it would be better to use 
+        // some kind of sequence. This is a stretch goal. 
+        $pulledValue = mysqli_fetch_array(mysqli_query($conn, "SELECT MAX(bookID) FROM zippybooksdb"));
+        
+        $id = $pulledValue[0]  + 1; 
+        
+        // if we have a qty of 0 or less, we want to set flag to true, and false otherwise 
+        if($qty > 0)
+        {
+            $query = "insert into zippybooksdb values($id, '$title', $price, $qty, 1)";
+        }
+        else
+        {
+            $query = "insert into zippybooksdb values($id, '$title', $price, $qty, 0)";
+        }
+        $result = mysqli_query($conn,$query);
+        
+        //echo $query;
+    }
+    elseif(isset($_POST['btn-price']))
+    {
+        // find if book exists 
+        $title = $_POST["priceform_Title"];
+        $price = $_POST["priceform_Price"];
+                
+        //$query = "SELECT * FROM zippybooksdb WHERE title = \"$title\"";
+        //$query = "SELECT title FROM zippybooksdb WHERE title = Arguments for Utilitarianism";
+    
+        $query = "SELECT title FROM zippybooksdb WHERE title = \"$title\"";
+
+        $checkPrice = mysqli_query($conn, $query);
+    
+        // if the title exists 
+        if($checkPrice)
+        {
+            // TODO: Why does zippybooksdb work, but \"$dbName\" or $dbName doesnt?
+            //$query = "UPDATE $dbName SET price = $price WHERE title = \"$title\"";
+            $query = "UPDATE zippybooksdb SET price = $price WHERE title = \"$title\"";
+            mysqli_query($conn, $query);
+        }
+    }
+    elseif(isset($_POST['btn-delete']))
+    {
+        // find if book exists 
+        $title = $_POST["deleteform_Title"];
+        $query = "SELECT title FROM zippybooksdb WHERE title = \"$title\"";
+        $checkPrice = mysqli_query($conn, $query);
+    
+        // if the title exists, delete it 
+        if($checkPrice)
+        {
+            $query = "DELETE FROM zippybooksdb WHERE title = \"$title\"";
+            mysqli_query($conn, $query);
+        }
+    }
+
+
+?>
